bbeb953f2ec0ecaef0d5da560d10ae2b8415154e
chetan882777 <chetan882777@gmail.com>
2019-05-28 15:42:55 +0530

    add view model factory module

--- app/src/main/java/com/chetan/stackoverflow/di/AppComponent.java
+++ app/src/main/java/com/chetan/stackoverflow/di/AppComponent.java
@@ -18,6 +18,7 @@ import dagger.android.support.AndroidSupportInjectionModule;
                 AndroidSupportInjectionModule.class,
                 ActivityBuildersModule.class,
                 AppModule.class,
+                ViewModelFactoryModule.class,
         })
 public interface AppComponent extends AndroidInjector<BaseApplication> {
 
