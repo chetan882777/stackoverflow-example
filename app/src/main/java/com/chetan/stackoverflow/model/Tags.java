4a8010aebf7a050c64bf4d7c575209287f56ba55
chetan882777 <chetan882777@gmail.com>
2019-05-29 00:11:24 +0530

    Tags model class

---
+++ app/src/main/java/com/chetan/stackoverflow/model/Tags.java
@@ -0,0 +1,49 @@
+package com.chetan.stackoverflow.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Tags {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("count")
+    @Expose
+    private int count;
+
+    @SerializedName("has_synonyms")
+    @Expose
+    private boolean hasSynonyms;
+
+    public Tags(String name, int count, boolean hasSynonyms) {
+        this.name = name;
+        this.count = count;
+        this.hasSynonyms = hasSynonyms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isHasSynonyms() {
+        return hasSynonyms;
+    }
+
+    public void setHasSynonyms(boolean hasSynonyms) {
+        this.hasSynonyms = hasSynonyms;
+    }
+}
--- app/src/main/java/com/chetan/stackoverflow/ui/tags/TagsViewModel.java
+++ app/src/main/java/com/chetan/stackoverflow/ui/tags/TagsViewModel.java
@@ -1,10 +1,12 @@
 package com.chetan.stackoverflow.ui.tags;
 
+import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.chetan.stackoverflow.model.Tags;
 import com.chetan.stackoverflow.network.tags.TagsApi;
 import com.chetan.stackoverflow.ui.Resource;
 
@@ -15,6 +17,8 @@ public class TagsViewModel extends ViewModel {
     private static final String TAG = "TagsViewModel";
 
     private final TagsApi tagsApi;
+
+    private MediatorLiveData<Resource<Tags>> tags = new MediatorLiveData<>();
 
     @Inject
     public TagsViewModel(TagsApi tagsApi){
