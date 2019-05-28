4588dfffc69c76675b065f1173bdc78b72ae3572
chetan882777 <chetan882777@gmail.com>
2019-05-28 15:40:56 +0530

    ViewModelProviderFactory

---
+++ app/src/main/java/com/chetan/stackoverflow/di/ViewModelFactoryModule.java
@@ -0,0 +1,17 @@
+package com.chetan.stackoverflow.di;
+
+
+import androidx.lifecycle.ViewModelProvider;
+
+import com.chetan.stackoverflow.viewmodels.ViewModelProviderFactory;
+
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class ViewModelFactoryModule {
+
+    @Binds
+    public abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelProviderFactory viewModelFactory);
+
+}
