fa3c282b802b6d6550156eff9ef37116005c2297
chetan882777 <chetan882777@gmail.com>
2019-05-28 14:34:36 +0530

    Activity builders module @contributesAndroidInjector

---
+++ app/src/main/java/com/chetan/stackoverflow/di/ActivityBuildersModule.java
@@ -0,0 +1,16 @@
+package com.chetan.stackoverflow.di;
+
+
+import com.chetan.stackoverflow.ui.auth.AuthActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+
+}
---
+++ app/src/main/java/com/chetan/stackoverflow/ui/auth/AuthActivity.java
@@ -0,0 +1,6 @@
+package com.chetan.stackoverflow.ui.auth;
+
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class AuthActivity extends DaggerAppCompatActivity {
+}
