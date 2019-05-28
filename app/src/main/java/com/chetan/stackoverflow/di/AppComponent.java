f7081cccd14202c7553a32993595b5ee80c3ef4e
chetan882777 <chetan882777@gmail.com>
2019-05-28 14:29:40 +0530

    Base application and application component

---
+++ app/src/main/java/com/chetan/stackoverflow/di/AppComponent.java
@@ -0,0 +1,32 @@
+package com.chetan.stackoverflow.di;
+
+
+import android.app.Application;
+
+import com.chetan.stackoverflow.BaseApplication;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Singleton
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        })
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
+
