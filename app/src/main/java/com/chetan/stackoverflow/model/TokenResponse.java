35259808a4760018a1269ee019d8c89e97f980a9
chetan882777 <chetan882777@gmail.com>
2019-05-28 19:53:18 +0530

    Added retrofit and rx dependencies. Added token model

---
+++ app/src/main/java/com/chetan/stackoverflow/model/TokenResponse.java
@@ -0,0 +1,42 @@
+package com.chetan.stackoverflow.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class TokenResponse {
+
+    @SerializedName("token_type")
+    @Expose
+    private String tokenType;
+    @SerializedName("expires_in")
+    @Expose
+    private Integer expiresIn;
+    @SerializedName("access_token")
+    @Expose
+    private String accessToken;
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
+
